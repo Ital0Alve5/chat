d5616335668c6a0bb240d9c99ef03bfe4276a9b2
Ital0Alve5 <italo_alves_@outlook.com>
2022-06-12 12:50:06 -0300

    chat

---
+++ chat/src/components/message/Status.vue
@@ -0,0 +1,79 @@
+<template>
+    <div class="warning">
+        <span :class="message.status == 'left' ? 'warning__left' : 'warning__on'">{{ message.user }} {{ message.status
+        }}</span>
+    </div>
+</template>
+
+<script>
+
+export default {
+    name: 'Received',
+    props: {
+        message: {
+            type: Object,
+            required: true
+        }
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+.warning {
+    position: relative;
+
+    &__left {
+        color: #dddddd;
+        font-weight: 700;
+
+        &::before {
+            content: ' ';
+            width: 100px;
+            height: 2px;
+            background: #dddddd;
+            position: absolute;
+            bottom: 50%;
+            right: 50%;
+            transform: translate(-50%, 100%);
+        }
+
+        &::after {
+            content: ' ';
+            width: 100px;
+            height: 2px;
+            background: #dddddd;
+            position: absolute;
+            bottom: 50%;
+            left: 50%;
+            transform: translate(50%, 100%);
+        }
+    }
+
+    &__on {
+        color: #94de94;
+        font-weight: 700;
+
+        &::before {
+            content: ' ';
+            width: 100px;
+            height: 2px;
+            background: #94de94;
+            position: absolute;
+            bottom: 50%;
+            right: 50%;
+            transform: translate(-50%, 100%);
+        }
+
+        &::after {
+            content: ' ';
+            width: 100px;
+            height: 2px;
+            background: #94de94;
+            position: absolute;
+            bottom: 50%;
+            left: 50%;
+            transform: translate(50%, 100%);
+        }
+    }
+}
+</style>
---
+++ chat/src/services/socketio.service.js
@@ -0,0 +1,111 @@
+import {
+    io
+} from 'socket.io-client';
+
+class SocketioService {
+    socket;
+
+    async asyncEmit(eventName, data) {
+        return new Promise((resolve, reject) => {
+            this.socket.emit(eventName, data);
+            this.socket.on(eventName, result => {
+                this.socket.off(eventName);
+                resolve(result);
+            });
+            setTimeout(reject, 1000);
+        });
+    }
+
+    setId() {
+        localStorage.setItem('userId', this.socket.id)
+    }
+
+    setupSocketConnection() {
+        this.socket = io(process.env.VUE_APP_SOCKET_ENDPOINT);
+    }
+
+    reconnect() {
+        this.socket.connect()
+    }
+    checkConnection() {
+        return this.socket.id
+    }
+
+    disconnect() {
+        if (this.socket) {
+            this.socket.disconnect();
+        }
+    }
+
+    emitMessage(value) {
+        if (this.socket) {
+            this.socket.emit("sendMessage", value)
+        }
+    }
+
+    async setTyping(store) {
+        if (this.socket) {
+            this.socket.emit('typing', {
+                user: store.user,
+                typing: store.typing,
+            })
+        }
+    }
+
+    getTyping(store) {
+        let waitStopTyping = undefined
+        if (this.socket) {
+            this.socket.on("typing", valueTyped => {
+                if (waitStopTyping != undefined) {
+                    clearTimeout(waitStopTyping)
+                }
+                waitStopTyping = setTimeout(() => {
+                    store.othersAreTyping = false
+                }, 800)
+
+                store.othersAreTyping = valueTyped
+            })
+        }
+    }
+
+    listenForMessages(store) {
+        if (this.socket) {
+            this.socket.on('receivedMessage', (message) => {
+                store.setMessages(message)
+            })
+        }
+    }
+
+    previousMessages(store) {
+        if (this.socket) {
+            this.socket.on('previousMessages', (value) => {
+                store.messages = []
+                for (let message of value) {
+                    store.setMessages(message)
+                }
+            })
+        }
+    }
+
+    async checkUser(user) {
+        if (this.socket) {
+            const userExist = await this.asyncEmit('checkIfUserExists', user)
+            return userExist
+        }
+    }
+
+    registerUser(user) {
+        if (this.socket) {
+            this.socket.emit('registerUser', user)
+        }
+    }
+
+    exit() {
+        if (this.socket) {
+            this.socket.emit('exit')
+        }
+    }
+
+}
+
+export default new SocketioService();
